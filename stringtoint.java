35b2220e069749c8745592e7ed34e39652e94c6d
Rajalakshmivenkat <rajivenkat.ece@gmail.com>
2016-07-26 15:48:04 +0530

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+public class Stringtoint
+{
+public static void main(String[] args)
+{
+String strval="1234";
+integer intval=integer.parseint(strval);
+Scanner sc=new Scanner(System.in);
+System.out.println("the integer is"+intval);
+}
+}
